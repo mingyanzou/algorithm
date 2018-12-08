c48736da82405e2b9a81e80a7a069a01dd017fa5
mingyanzou <mingyan.zmy@gmail.com>
2018-12-08 14:57:56 +0800

    add insert sort

--- src/com/zmy/basic/sort/InsertSort.java
+++ src/com/zmy/basic/sort/InsertSort.java
@@ -1,4 +1,27 @@
 package com.zmy.basic.sort;
 
 public class InsertSort {
+    public static void main(String args[]) {
+        int[] a = {6, 8, 4, 9, 3, 5, 7, 1, 2};
+        sort(a);
+    }
+
+    private static void sort(int[] arrays) {
+        for (int i = 1; i < arrays.length; i++) {
+            int value = arrays[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (arrays[j] > value) {
+                    arrays[j + 1] = arrays[j];
+                } else {
+                    break;
+                }
+            }
+            arrays[j + 1] = value;
+        }
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.print(arrays[i] + " ");
+        }
+    }
+
 }
