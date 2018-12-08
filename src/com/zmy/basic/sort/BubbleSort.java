3cdfa47ab1596a8c9e38daed6334679cbe1cb429
mingyanzou <mingyan.zmy@gmail.com>
2018-12-08 16:09:01 +0800

    向下冒泡排序

--- src/com/zmy/basic/sort/BubbleSort.java
+++ src/com/zmy/basic/sort/BubbleSort.java
@@ -4,25 +4,42 @@ public class BubbleSort {
 
     public static void main(String args[]) {
         int[] a = {6, 8, 4, 9, 3, 5, 7, 1, 2};
-        sort(a);
+//        bubbleDownSort(a);
+        bubbleSort(a);
     }
 
-    private static void sort(int[] arrays) {
+    private static void bubbleDownSort(int[] arrays) {
         for (int i = 0; i < arrays.length - 1; i++) {
-            boolean isChanging = false;
             for (int j = i + 1; j < arrays.length; j++) {
                 if (arrays[i] > arrays[j]) {
                     int temp = arrays[i];
                     arrays[i] = arrays[j];
                     arrays[j] = temp;
                 }
-                isChanging = true;
             }
-            if (!isChanging) break;
         }
 
         for (int i = 0; i < arrays.length; i++) {
             System.out.print(arrays[i] + " ");
+        }
+    }
+
+    private static void bubbleSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+                flag = true;
+            }
+            if (!flag) break;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
         }
     }
 
