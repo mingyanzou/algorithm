73b89cfbd357f381fb005c2fa2dfd1fe16437c7b
mingyanzou <mingyan.zmy@gmail.com>
2019-04-13 17:08:39 +0800

    二叉树查找、插入

---
+++ src/com/zmy/basic/Binarytree/BinarySearchTree.java
@@ -0,0 +1,49 @@
+package com.zmy.basic.Binarytree;
+
+public class BinarySearchTree {
+    private Node tree;
+
+    public Node find(int data){
+        Node p = tree;
+        while (p != null){
+            if (p.data == data) return p;
+            if (data < p.data) p =  p.left;
+            else if (data > p.data) p = p.right;
+        }
+        return null;
+    }
+
+    public void insert(int data){
+        if (tree == null){
+            tree = new Node(data);
+            return;
+        }
+        Node p = tree;
+        while (p != null){
+            if (data > p.data){
+                if (p.right == null){
+                    p.right = new Node(data);
+                    return;
+                }
+                p = p.right;
+            } else {
+                if (p.left == null){
+                    p.left = new Node(data);
+                    return;
+                }
+                p = p.left;
+            }
+        }
+    }
+
+
+    public class Node{
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+}
